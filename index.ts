a489dbfc0f9ff5499ccd0a298edf61f15a82e983
Clark Bynum <cbynum@clearblade.com>
2019-01-26 23:05:13 -0600

    construct types programmatically

--- index.ts
+++ index.ts
@@ -1,143 +1,57 @@
 import express, { RequestHandler } from "express";
 import graphqlHTTP from "express-graphql";
-import { buildSchema } from "graphql";
+import { GraphQLObjectType, GraphQLString, GraphQLSchema } from "graphql";
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-    rollDice(numDice: Int!, numSides: Int): [Int]
-    getDie(numSides: Int): RandomDie
-    getMessage(id: ID!): Message
-  }
-
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
-  }
-
-  input MessageInput {
-    content: String
-    author: String
-  }
-
-  type Message {
-    id: ID!
-    content: String
-    author: String
-  }
-
-  type Mutation {
-    createMessage(input: MessageInput): Message
-    updateMessage(id: ID!, input: MessageInput): Message
-  }
-`);
-
-// This class implements the RandomDie GraphQL type
-class RandomDie {
-  numSides: number;
-  constructor(numSides: number) {
-    this.numSides = numSides;
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
-
-  roll({ numRolls }: { numRolls: number }) {
-    const output = [];
-    for (let i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
-  }
-}
-
-interface MessageInput {
-  content: string;
-  author: string;
-}
-
-// If Message had any complex fields, we'd put them on this object.
-class Message {
+interface IUser {
   id: string;
-  content: string;
-  author: string;
-  constructor(id: string, { content, author }: MessageInput) {
-    this.id = id;
-    this.content = content;
-    this.author = author;
-  }
+  name: string;
 }
 
-const fakeDatabase: { [id: string]: MessageInput } = {};
-// The root provides a resolver function for each API endpoint
-const root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
+// Maps id to User object
+var fakeDatabase: { [id: string]: IUser } = {
+  a: {
+    id: "a",
+    name: "alice"
   },
-  random: () => {
-    return Math.random();
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-  },
-  rollDice: function({
-    numDice,
-    numSides
-  }: {
-    numDice: number;
-    numSides: number;
-  }) {
-    const output = [];
-    for (let i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-    }
-    return output;
-  },
-  getDie: function({ numSides }: { numSides: number }) {
-    return new RandomDie(numSides || 6);
-  },
-  getMessage: function({ id }: { id: string }) {
-    if (!fakeDatabase[id]) {
-      throw new Error("no message exists with id " + id);
-    }
-    return new Message(id, fakeDatabase[id]);
-  },
-  createMessage: function({ input }: { input: MessageInput }) {
-    // Create a random id for our "database".
-    var id = require("crypto")
-      .randomBytes(10)
-      .toString("hex");
-
-    fakeDatabase[id] = input;
-    return new Message(id, input);
-  },
-  updateMessage: function({ id, input }: { input: MessageInput; id: string }) {
-    if (!fakeDatabase[id]) {
-      throw new Error("no message exists with id " + id);
-    }
-    // This replaces all old data, but some apps might want partial update.
-    fakeDatabase[id] = input;
-    return new Message(id, input);
+  b: {
+    id: "b",
+    name: "bob"
   }
 };
 
-const loggingMiddleware: RequestHandler = (req, res, next) => {
-  console.log("ip:", req.ip);
-  next();
-};
+// Define the User type
+var userType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString }
+  }
+});
+
+// Define the Query type
+var queryType = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    user: {
+      type: userType,
+      // `args` describes the arguments that the `user` query accepts
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve: function(_, { id }) {
+        return fakeDatabase[id];
+      }
+    }
+  }
+});
+
+var schema = new GraphQLSchema({ query: queryType });
 
 var app = express();
-app.use(loggingMiddleware);
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true
   })
 );
