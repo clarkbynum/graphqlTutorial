0bf84ad2c0d239aa418a397d2c6d6a3ddd43c2fb
Clark Bynum <cbynum@clearblade.com>
2019-01-26 22:48:52 -0600

    mutations

--- index.ts
+++ index.ts
@@ -10,12 +10,29 @@ var schema = buildSchema(`
     rollThreeDice: [Int]
     rollDice(numDice: Int!, numSides: Int): [Int]
     getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
   }
 
   type RandomDie {
     numSides: Int!
     rollOnce: Int!
     roll(numRolls: Int!): [Int]
+  }
+
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `);
 
@@ -31,16 +48,34 @@ class RandomDie {
   }
 
   roll({ numRolls }: { numRolls: number }) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
+    const output = [];
+    for (let i = 0; i < numRolls; i++) {
       output.push(this.rollOnce());
     }
     return output;
   }
 }
 
+interface MessageInput {
+  content: string;
+  author: string;
+}
+
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  id: string;
+  content: string;
+  author: string;
+  constructor(id: string, { content, author }: MessageInput) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+const fakeDatabase: { [id: string]: MessageInput } = {};
 // The root provides a resolver function for each API endpoint
-var root = {
+const root = {
   quoteOfTheDay: () => {
     return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
   },
@@ -57,14 +92,37 @@ var root = {
     numDice: number;
     numSides: number;
   }) {
-    var output = [];
-    for (var i = 0; i < numDice; i++) {
+    const output = [];
+    for (let i = 0; i < numDice; i++) {
       output.push(1 + Math.floor(Math.random() * (numSides || 6)));
     }
     return output;
   },
   getDie: function({ numSides }: { numSides: number }) {
     return new RandomDie(numSides || 6);
+  },
+  getMessage: function({ id }: { id: string }) {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id " + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: function({ input }: { input: MessageInput }) {
+    // Create a random id for our "database".
+    var id = require("crypto")
+      .randomBytes(10)
+      .toString("hex");
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function({ id, input }: { input: MessageInput; id: string }) {
+    if (!fakeDatabase[id]) {
+      throw new Error("no message exists with id " + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
   }
 };
 
